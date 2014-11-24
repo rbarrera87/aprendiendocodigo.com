70ac1907bbab1c1b01b73a4ebef568b202c57ec4
Richard <rbarrera87@gmail.com>
2014-11-23 18:45:45 -0800

    adding categories model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141122040210) do
+ActiveRecord::Schema.define(version: 20141124023844) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name",        null: false
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "registers", force: true do |t|
     t.string   "name"
