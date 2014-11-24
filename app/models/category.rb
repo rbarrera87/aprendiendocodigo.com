b14af1b464b4a4466e3cb56cd9b2f6a1ca949bbf
Richard <rbarrera87@gmail.com>
2014-11-23 21:36:26 -0800

    new Course model and new validations for it

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ActiveRecord::Base
+  has_many :courses
   validates_presence_of :name
 end
---
+++ spec/factories/courses.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :course do
+    name "MyString"
+description "MyString"
+reference ""
+  end
+
+end
